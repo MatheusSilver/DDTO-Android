b2932ad5cd71dd73980e9b0b788ac99baee4c301
Random people <98900217+justaguy6@users.noreply.github.com>
2023-03-20 09:32:26 -0300

    Update WarpShader.hx

--- source/shaders/WarpShader.hx
+++ source/shaders/WarpShader.hx
@@ -8,19 +8,19 @@ class WarpShader extends FlxShader // modified from https://www.shadertoy.com/vi
 	#pragma header
 	uniform float iTime;
 
-	float transformStrength = 0.4;
+	float transformStrength = vec2(0.4);
 
 	vec4 perm(vec4 x)
 	{
-		x = ((x * 34.0) + 1.0) * x;
-		return x - floor(x * (1.0 / 289.0)) * 289.0;
+		x = ((x * 34.0) + 1.0) * vec4(x);
+		return x - floor(x * (1.0 / vec4(289.0))) * vec2(289.0);
 	}
 
 	float noise2d(vec2 p)
 	{
 		vec2 a = floor(p);
 		vec2 d = p - a;
-		d = d * d * (3.0 - 2.0 * d);
+		d = d * d * (3.0 - vec2(2.0) * vec4(d));
 
 		vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
 		vec4 k1 = perm(b.xyxy);
@@ -30,8 +30,8 @@ class WarpShader extends FlxShader // modified from https://www.shadertoy.com/vi
 		vec4 k3 = perm(c);
 		vec4 k4 = perm(c + 1.0);
 
-		vec4 o1 = fract(k3 * 0.0244);
-		vec4 o2 = fract(k4 * 0.0244);
+		vec4 o1 = fract(k3 * vec2(0.0244));
+		vec4 o2 = fract(k4 * vec2(0.0244));
 
 		vec4 o3 = o2 * d.y + o1 * (1.0 - d.y);
 		vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
@@ -43,8 +43,8 @@ class WarpShader extends FlxShader // modified from https://www.shadertoy.com/vi
 	{
 		vec2 uv = openfl_TextureCoordv.xy;
 
-		uv.x -= 0.05;
-		uv.y -= 0.05;
+		uv.x -= vec2(0.05);
+		uv.y -= vec2(0.05);
 
 		float v1 = noise2d(vec2(uv * transformStrength - iTime));
 		float v2 = noise2d(vec2(uv * transformStrength + iTime));
