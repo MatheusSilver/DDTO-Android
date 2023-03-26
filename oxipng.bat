bbc6ccb09bf6e1bf8a54c6aea8e4a80fdf8b24b1
Mateus Soares <105178173+MateuzinhoX02@users.noreply.github.com>
2023-03-26 08:50:00 -0300

    Literal, a otimização de TODAS as IMAGENS do jogo
    (Isso me demorou muito tempo, mas se precisar remover, só fazer um revert)

--- oxipng.bat
+++ oxipng.bat
@@ -1,2 +1,2 @@
 @REM Download oxipng from https://github.com/shssoichiro/oxipng/releases/latest, copy it in the root folder of the project, then run this bat file.
-oxipng -r assets/doki/images/libitina/
+oxipng -r assets/
